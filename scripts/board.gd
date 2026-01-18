ac547c8229179efac7b788bb182a7e98277f0640
OmarBouhlal <omarbouhlal05@gmail.com>
2026-01-18 12:43:13 +0100

    feat : base logic done

--- scripts/board.gd
+++ scripts/board.gd
@@ -5,6 +5,9 @@ const CELL_W = 20
 
 const TEXTURE_HOLDER = preload("res://scenes/texture_holder.tscn")
 
+# Possible move
+const PIECE_MOVE = preload("res://art/dot/piece_move.png")
+
 # Bishops
 const BLACK_BISHOP = preload("res://art/bishop/black_bishop.png")
 const WHITE_BISHOP = preload("res://art/bishop/white_bishop.png")
@@ -25,11 +28,12 @@ const BLACK_ROOK = preload("res://art/rook/black_rook.png")
 const WHITE_ROOK = preload("res://art/rook/white_rook.png")
 
 @onready var pieces: Node2D = $pieces
+@onready var dots: Node2D = $dots
 
 var board: Array
-var white : bool
-var state : bool
-var seleted_piece : Vector2
+var white : bool = true
+var state : bool = false
+var selected_piece : Vector2
 var moves = []
 
 # number -> type of piece and color
@@ -58,26 +62,208 @@ func _ready() -> void:
 
 
 func draw_board():
+	for child in pieces.get_children():
+		child.queue_free()
 	for i in BOARD_SIZE:
 		for j in BOARD_SIZE:
 			var piece_holder:Sprite2D = TEXTURE_HOLDER.instantiate()
 			pieces.add_child(piece_holder)
-			piece_holder.global_position = Vector2(j*CELL_W + CELL_W / 2 , - i * CELL_W - CELL_W / 2 )
+			piece_holder.global_position = Vector2(j*CELL_W + floor(CELL_W / 2) , - i * CELL_W - floor(CELL_W / 2) )
 			
 			match board[i][j]:
+				# BLACK Player :
 				-6: piece_holder.texture = BLACK_PAWN
 				-5: piece_holder.texture = BLACK_ROOK
 				-4: piece_holder.texture = BLACK_KNIGHT
 				-3: piece_holder.texture = BLACK_BISHOP
 				-2: piece_holder.texture = BLACK_QUEEN
 				-1: piece_holder.texture = BLACK_KING
+				# no piece:
 				0: piece_holder.texture = null
+				# WHITE Player :
 				1: piece_holder.texture = WHITE_KING
 				2: piece_holder.texture = WHITE_QUEEN
 				3: piece_holder.texture = WHITE_BISHOP
 				4: piece_holder.texture = WHITE_KNIGHT
 				5: piece_holder.texture = WHITE_ROOK
 				6: piece_holder.texture = WHITE_PAWN
+
+func _input(event):
+	if event is InputEventMouseButton && event.pressed:
+		if event.button_index  == MOUSE_BUTTON_LEFT:
+			if is_mouse_out():
+				print("out")
+				return 
+			var var1 = int(get_global_mouse_position().x / CELL_W)
+			var var2 = abs(int(get_global_mouse_position().y / CELL_W))
+			print(var1,var2)
+			if !state && (white && board[var2][var1] > 0 || !white && board[var2][var1] < 0) :
+				print("valid select of piece") 
+				selected_piece = Vector2(var2,var1)
+				state = true
+				show_options()
+			elif state:
+				# the move (action) is confirmed
+				set_move(var2,var1)
+				
+				
+				
+
+func show_options():
+	moves = get_actions()
+	if moves == []:
+		state = false
+		return
+	show_dots()
+func show_dots():
+	for i in moves:
+		var holder:Sprite2D = TEXTURE_HOLDER.instantiate()
+		dots.add_child(holder)
+		holder.texture = PIECE_MOVE
+		holder.global_position = Vector2(i.y * CELL_W + floor(CELL_W/2),- i.x * CELL_W - floor(CELL_W/2))
+		
+func delete_dots():
+	for child in dots.get_children():
+		child.queue_free()		
+func set_move(var2,var1):
+		for i in moves:
+			if i.x == var2 && i.y == var1:
+				board[var2][var1] = board[selected_piece.x][selected_piece.y]
+				board[selected_piece.x][selected_piece.y] = 0
+				white = !white
+				draw_board()	
+				break	
+		delete_dots()
+		state = false			
+func get_actions():
+	var _moves= []
+	match abs(board[selected_piece.x][selected_piece.y]):
+		1:_moves = get_king_actions()
+		2:_moves = get_queen_actions()
+		3:_moves = get_bishop_actions()
+		4:_moves = get_knight_actions()
+		5:_moves = get_rook_actions()
+		6:_moves = get_pawn_actions()
+	return _moves
+func get_rook_actions():
+	var _moves = []
+	var directions = [Vector2(0,1),Vector2(0,-1),Vector2(1,0),Vector2(-1,0)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		while is_valid_position(pos):
+			if is_empty(pos): _moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+				break
+			else:
+				break
+			pos+=i		
+	return _moves
+
+func get_bishop_actions():
+	var _moves = []
+	var directions = [Vector2(1,1),Vector2(1,-1),Vector2(-1,-1),Vector2(-1,1)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		while is_valid_position(pos):
+			if is_empty(pos): _moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+				break
+			else:
+				break
+			pos+=i		
+	return _moves
+
+func get_queen_actions():
+	var _moves = []
+	var directions = [Vector2(1,1),Vector2(1,-1),Vector2(-1,-1),Vector2(-1,1),Vector2(0,1),Vector2(0,-1),Vector2(1,0),Vector2(-1,0)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		while is_valid_position(pos):
+			if is_empty(pos): _moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+				break
+			else:
+				break
+			pos+=i		
+	return _moves
+	
+func get_king_actions():
+	var _moves = []
+	var directions = [Vector2(1,1),Vector2(1,-1),Vector2(-1,-1),Vector2(-1,1),Vector2(0,1),Vector2(0,-1),Vector2(1,0),Vector2(-1,0)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		if is_valid_position(pos):
+			if is_empty(pos): _moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+	return _moves			
+
+func get_knight_actions():
+	var _moves = []
+	var directions = [Vector2(2,1),Vector2(2,-1),Vector2(1,2),Vector2(-1,2),Vector2(-2,1),Vector2(-2,-1),Vector2(1,-2),Vector2(-1,-2)]
+	
+	for i in directions:
+		var pos = selected_piece
+		pos += i
+		if is_valid_position(pos):
+			if is_empty(pos): _moves.append(pos)
+			elif is_enemy(pos):
+				_moves.append(pos)
+	return _moves	
+	
+func get_pawn_actions():
+	var _moves = []
+	var direction
+	var is_first_move = false
+	if white: direction = Vector2(1,0)
+	else:
+		direction = Vector2(-1,0)
+	
+	if white && selected_piece.x == 1 || !white && selected_piece.x == 6:
+		is_first_move = true
+	var pos = selected_piece + direction
+	if is_empty(pos): _moves.append(pos)
+	pos = selected_piece + Vector2(direction.x,1)
+	if is_valid_position(pos):
+		if is_enemy(pos): _moves.append(pos)
+	pos = selected_piece + Vector2(direction.x,-1)
+	if is_valid_position(pos):
+		if is_enemy(pos): _moves.append(pos)
+		
+	pos  = selected_piece + direction * 2
+	if is_first_move && is_empty(pos) && is_empty(selected_piece + direction):
+		_moves.append(pos)
+	
+	return _moves	
 			
+func is_valid_position(pos:Vector2):
+	if pos.x >= 0 && pos.x < BOARD_SIZE && pos.y >=0 && pos.y < BOARD_SIZE: return true
+	return false
+func is_empty(pos:Vector2):
+	if board[pos.x][pos.y] == 0: return true
+	return false	
+func is_enemy(pos:Vector2):
+	if white && board[pos.x][pos.y] < 0 || !white && board[pos.x][pos.y] > 0: return true
+	return false		
+			
+		
+func is_mouse_out():
+	if get_global_mouse_position().x  < 0 || get_global_mouse_position().y > 0 || get_global_mouse_position().y < -160 || get_global_mouse_position().x > 160 :	
+		return true
+	return false	
+				
+			
+				  
 func _process(delta: float) -> void:
 	pass
